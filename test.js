6784b5f28366b58b48549e919331094000d3294f
Josh-Weidner <josh.weidner2@gmail.com>
2024-02-24 17:40:42 -0700

    made game states for when you lose and win.

--- test.js
+++ test.js
@@ -5,32 +5,73 @@ import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
 // initialize kaboom context
 kaboom();
 
+
+
 loadSprite("background", "clouds.jpeg.webp");
-add([
-    sprite("background"),
-    scale(3.3,3.8),
-    pos(0, 0),
-]);
-
-// add a piece of text at position (120, 80)
-add([
-    text("Score: "),
-    pos(800, 40),
-]);
-
 loadSprite("basketball", "basketball.png")
+loadSprite("hoop", "hoop.png")
 
-const basketball = add([
-    sprite("basketball"),
-    pos(100, 400),
-    scale(.12),
-    area(),
-])
+scene("game", () => {
+    add([
+        sprite("background"),
+        scale(3.3,3.8),
+        pos(0, 0),
+    ]);
+    
+    const basketball = add([
+        sprite("basketball"),
+        pos(100, 400),
+        scale(.12),
+        area(),
+    ])
+    
+    loop(2, () => {
+        const hoop = add([
+            sprite("hoop"),
+            pos(1800, rand(100, 1000)),
+            rotate(270),
+            scale(.8),
+            move(LEFT, 800),
+            area(),
+            "hoop"
+        ])
+    })
+    
+    onKeyDown("down", () => {
+        basketball.move(0, 1000)
+    })
+    
+    onKeyDown("up", () => {
+        basketball.move(0, -1000)
+    })
 
-onKeyPress("down", () => {
-    basketball.move(0, 3000)
-})
+    let score = 0;
+    const scoreLabel = add([
+        text("Score: " + score),
+        pos(800, 40)
+    ])
 
-onKeyPress("up", () => {
-    basketball.move(0, -3000)
-})
+    basketball.onCollide("hoop", () => {
+        score = score + 1;
+        go("lose", score);
+    });
+
+    onUpdate(() => {
+        scoreLabel.text = "Score: " + score;
+    });
+});
+
+scene("lose", (score) => {
+    add([
+        text(score),
+        pos(width() / 2, height() / 2 + 80),
+        scale(2),
+        anchor("center"),
+    ]);
+
+    onKeyPress("up", () => go("game"));
+    onKeyPress("down", () => go("game"));
+
+});
+
+go("game")
