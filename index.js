5138af60820b151848ed914b75ca898efeee3919
Josh-Weidner <josh.weidner2@gmail.com>
2024-03-10 23:50:47 -0600

    finished back end mthods and endpoints

--- index.js
+++ index.js
@@ -6,20 +6,54 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+// create user
+app.post('/createuser', (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.send("user created");
+})
+
 // pull high scores
 app.get('/highscore', (req, res) => {res.send(highScores)})
 
+// pull recent scores
+app.get('recentscores', (req, res) => {res.send(recentScores)})
+
 // update high scores
-app.post('/updatehigh', (req, res) => {
-    highScores = req.body;
+app.put('/updatehigh', (req, res) => {
+    const score = req.body;
+    if (score.score > highScores[2].score && score.score < highScores[1].score) {
+        highScores[2] = score;
+    }
+    if (score.score > highScores[1].score && score.score < highScores[0].score) {
+        highScores[2] = highScores[1];
+        highScores[1] = score;
+    }
+    if (score.score > highScores[0].score) {
+        highScores[2] = highScores[1];
+        highScores[1] = highScores[0];
+        highScores[0] = score;
+    }
+    res.send("scores updated appropriately");
 })
 
 // update recent score
 app.put('/updaterecent', (req, res) => {
-    const newRecent = req.body;
+    const score = req.body;
     recentScores[2] = recentScores[1];
     recentScores[1] = recentScores[0];
-    recentScores[0] = newRecent;
+    recentScores[0] = score;
+    res.send("recents updated correctly");
+})
+
+// update users high score
+app.put('/:username/:score', (req, res) => {
+    const userName = req.params.username;
+    const score = req.params.score;
+    const user = users.find(user => user.userName = userName);
+    if (user.highScore < score) {
+        user.highScore = score;
+    }
 })
 
 // launch the server on port 3000
